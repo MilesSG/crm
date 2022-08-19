16de00217bc91ce4916c7c34880fff084fa3b063
Miles <1290672153@qq.com>
2022-08-19 22:14:36 +0800

    Update BaseController.java

--- src/main/java/com/xxxx/crm/base/BaseController.java
+++ src/main/java/com/xxxx/crm/base/BaseController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import javax.servlet.http.HttpServletRequest;
 
 public class BaseController {
-
-
     @ModelAttribute
     public void preHandler(HttpServletRequest request){
         request.setAttribute("ctx", request.getContextPath());
